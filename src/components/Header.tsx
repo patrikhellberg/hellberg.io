ed876553b3cc8dd337ca19eefb25f81bb82c61c4
bucketmedia <61097620+bucketmedia@users.noreply.github.com>
2023-12-12 08:16:07 +0100

    remove header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,9 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='relative mb-16'>
+    <div className='mb-4'>
       <Image
-        src='/cover.jpeg'
-        alt='cover'
-        className='rounded md:block'
-        width={800}
-        height={100}
-        priority
-      />
-      <Image
-        className='rounded-full shadow-2xl sm:w-28 md:w-48 w-20 absolute -bottom-8 left-8'
+        className='rounded-full shadow-2xl sm:w-28 md:w-48 w-20'
         title='Patrik Hellberg'
         src='/portrait.jpeg'
         alt='Patrik Hellberg'
